698ee4617591e22f280cb8d702e8573ffbbd5cdc
rishabhjain <rishabhjain2795@gmail.com>
2014-07-24 00:01:22 +0300

    back to normal

--- lib/tgm.rb
+++ lib/tgm.rb
@@ -1,26 +1,18 @@
-require 'tgm/cli'
-require 'time'
+require "tgm/version"
+require "gmail"
+require "thor"
+require "shellwords"
 
 module Tgm
-  class << self
-    # Convert time to local time by applying the `utc_offset` setting.
-    def local_time(time)
-      utc_offset ? (time.dup.utc + utc_offset) : time.localtime
+  # Your code goes here...
+  class CLI < Thor 
+    desc 'login', 'Log into your gmail account.'
+    def login
     end
-
-    # UTC offset in seconds to apply time instances before displaying.
-    # If not set, time instances are displayed in default local time.
-    attr_reader :utc_offset
-
-    def utc_offset=(offset)
-      @utc_offset = case offset
-      when String
-        Time.zone_offset(offset)
-      when NilClass
-        nil
-      else
-        offset.to_i
-      end
+    desc 'closer', 'puts closer'
+    def closer
+      puts("Method Works: Closer")
     end
   end
 end
+
