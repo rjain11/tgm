1e8f8ab94a66f5d7eaf06f0c77345319ec187586
rishabhjain <rishabhjain2795@gmail.com>
2014-07-24 18:25:40 +0300

    gem not working.

--- lib/tgm.rb
+++ lib/tgm.rb
@@ -8,10 +8,20 @@ module Tgm
   class CLI < Thor 
     desc 'login', 'Log into your gmail account.'
     def login
-      username = ask 'Enter your username:'
-      password = ask 'Enter your password:'
-      $gmail = Gmail.connect(username, password)
+      $username = ask 'Enter your username:'
+      $password = ask 'Enter your password:'
+      $gmail = Gmail.connect($username, $password)
       say 'User Authorized!'
+      to_mail = ask 'Enter email-id:'
+      subject_mail = ask 'Enter subject:'
+      body_mail = ask 'Enter body:'
+      email=$gmail.compose do
+        to to_mail
+        subject subject_mail
+        body body_mail
+      end
+      $gmail.deliver(email)
+      $gmail.logout
     end
     desc 'mail', 'Send an email.'
     def mail
@@ -24,6 +34,7 @@ module Tgm
         body body_mail
       end
       $gmail.deliver(email)
+      $gmail.logout
     end
   end
 end
