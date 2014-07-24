1a5364add29d0ae2b4a814fb4fb731be9490ab4f
rishabhjain <rishabhjain2795@gmail.com>
2014-07-24 17:52:09 +0300

    New methods

--- lib/tgm.rb
+++ lib/tgm.rb
@@ -8,10 +8,22 @@ module Tgm
   class CLI < Thor 
     desc 'login', 'Log into your gmail account.'
     def login
+      username = ask 'Enter your username:'
+      password = ask 'Enter your password:'
+      $gmail = Gmail.connect(username, password)
+      say 'User Authorized!'
     end
-    desc 'closer', 'puts closer'
-    def closer
-      puts("Method Works: Closer")
+    desc 'mail', 'Send an email.'
+    def mail
+      to_mail = ask 'Enter email-id:'
+      subject_mail = ask 'Enter subject:'
+      body_mail = ask 'Enter body:'
+      email=$gmail.compose do
+        to to_mail
+        subject subject_mail
+        body body_mail
+      end
+      $gmail.deliver(email)
     end
   end
 end
