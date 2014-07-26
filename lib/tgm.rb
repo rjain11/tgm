f68511c6b3bd743c930bc55f00fc66ac24c55373
rishabhjain <rishabhjain2795@gmail.com>
2014-07-27 00:08:05 +0300

    changes. working individually.

--- lib/tgm.rb
+++ lib/tgm.rb
@@ -10,31 +10,47 @@ module Tgm
     def login
       $username = ask 'Enter your username:'
       $password = ask 'Enter your password:'
-      $gmail = Gmail.connect!($username, $password)
+      gmail = Gmail.connect!($username, $password)
       say 'User Authorized!'
-      to_mail = ask 'Enter email-id:'
-      subject_mail = ask 'Enter subject:'
-      body_mail = ask 'Enter body:'
-      email=$gmail.compose do
-        to to_mail
-        subject subject_mail
-        body body_mail
-      end
-      $gmail.deliver(email)
-      $gmail.logout
+      #to_mail = ask 'Enter email-id:'
+      #subject_mail = ask 'Enter subject:'
+      #body_mail = ask 'Enter body:'
+      #email=$gmail.compose do
+      #  to to_mail
+      #  subject subject_mail
+      #  body body_mail
+      #end
+      #$gmail.deliver(email)
+      #$gmail.logout
     end
     desc 'mail', 'Send an email.'
     def mail
+      $username = ask 'Enter your username:'
+      $password = ask 'Enter your password:'
+      gmail = Gmail.new($username, $password)
       to_mail = ask 'Enter email-id:'
       subject_mail = ask 'Enter subject:'
       body_mail = ask 'Enter body:'
-      email=$gmail.compose do
+      email=gmail.generate_message do
         to to_mail
         subject subject_mail
         body body_mail
       end
-      $gmail.deliver(email)
-      $gmail.logout
+      gmail.deliver(email)
+      gmail.logout
+    end
+    desc 'inbox', 'Get your inbox statistics'
+    def inbox
+      $username = ask 'Enter your username:'
+      $password = ask 'Enter your password:'
+      gmail = Gmail.new($username, $password)
+      noInbox=gmail.inbox.count
+      noUnread=gmail.inbox.count(:unread)
+      noRead=gmail.inbox.count(:read)
+      say 'Number of Mails: '+noInbox.to_s
+      say 'Number of Unread Mails: '+noUnread.to_s
+      say 'Number of Read Mails: '+noRead.to_s
+      gmail.logout
     end
   end
 end
