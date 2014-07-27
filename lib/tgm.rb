70a5b2915c615482e76b5e8dd9ebbecd53f4f07c
rishabhjain <rishabhjain2795@gmail.com>
2014-07-27 19:58:41 +0300

    added labels method

--- lib/tgm.rb
+++ lib/tgm.rb
@@ -12,16 +12,6 @@ module Tgm
       $password = ask 'Enter your password:'
       gmail = Gmail.connect!($username, $password)
       say 'User Authorized!'
-      #to_mail = ask 'Enter email-id:'
-      #subject_mail = ask 'Enter subject:'
-      #body_mail = ask 'Enter body:'
-      #email=$gmail.compose do
-      #  to to_mail
-      #  subject subject_mail
-      #  body body_mail
-      #end
-      #$gmail.deliver(email)
-      #$gmail.logout
     end
     desc 'mail', 'Send an email.'
     def mail
@@ -50,6 +40,16 @@ module Tgm
       say 'Number of Mails: '+noInbox.to_s
       say 'Number of Unread Mails: '+noUnread.to_s
       say 'Number of Read Mails: '+noRead.to_s
+      gmail.logout
+    end
+    desc 'labels', 'Get info about all your labels'
+    def labels
+      $username = ask 'Enter your username:'
+      $password = ask 'Enter your password:'
+      gmail = Gmail.new($username, $password)
+      labelname = ask 'Enter the label name: '
+      noEmailsInLabel=gmail.mailbox(labelname).count
+      say 'Number of mails in '+labelname.to_s+': '+noEmailsInLabel.to_s
       gmail.logout
     end
   end
